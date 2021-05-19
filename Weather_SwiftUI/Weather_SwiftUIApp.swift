219f64f5a85e5ac64b95c0488ba5d06f557df4d4
msalem <msalem@egypt.itgsolutions.com>
2021-05-19 14:57:24 +0200

    Initial Commit

---
+++ Weather_SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather_SwiftUI
+//
+//  Created by Mohamed Samir on 19/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
